f1e99a507f92ff96937c9a2f3a62a12c66c0685b
hoang <giahoangth@gmail.com>
2020-05-20 17:36:29 +0700

    c1-m1-findian

---
+++ c1-m2-activity/findian.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var findString string
+	fmt.Printf("Please input a string: ")
+	num, err := fmt.Scan(&findString)
+	if err != nil {
+		fmt.Printf(err.Error() + string(num))
+	}
+
+	if findian(findString) {
+		fmt.Printf("Found!\n")
+	} else {
+		fmt.Printf("Not Found!\n")
+	}
+
+}
+
+func findian(s string) bool {
+	if strings.ContainsAny(string(s[0]), "iI") && strings.ContainsAny(s, "aA") && strings.ContainsAny(string(s[len(s)-1]), "n") {
+		return true
+	}
+	return false
+}
