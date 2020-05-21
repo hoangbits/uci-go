8706857b4523e64414196ccf9cd315ff52ff7f98
hoang <giahoangth@gmail.com>
2020-05-21 12:00:00 +0700

    c1-m3-slice sorting with user input data

---
+++ c1-m3-activity/slice.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"unicode"
+)
+
+func main() {
+	sli := make([]int, 0, 3)
+	for {
+		var inputValue rune
+		fmt.Println("Please enter an integer: ")
+		// duplicated line: https://stackoverflow.com/questions/57734634/why-i-get-a-duplicate-output-when-the-loop-if-there-is-a-scanf
+		num, err := fmt.Scanf("%c\n", &inputValue)
+		if err != nil {
+			fmt.Print(err.Error() + "\n")
+			continue
+		}
+
+		if unicode.IsNumber(inputValue) {
+			// get integer out of ASCII: https://stackoverflow.com/questions/21322173/convert-rune-to-int
+			number := int(inputValue - '0')
+			sli = append(sli, number)
+			sort.SliceStable(sli, func(i, j int) bool { return sli[i] < sli[j] })
+
+			fmt.Printf("Scanned: %d len=%d cap=%d %v \n\n", num, len(sli), cap(sli), sli)
+
+		} else if unicode.IsLetter(inputValue) && inputValue == rune('w') {
+			fmt.Print("bye!\n")
+			break
+		}
+
+	}
+}
