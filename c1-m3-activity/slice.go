4579342701f318b86119a5f329bcd7e90265ae3e
hoang <giahoangth@gmail.com>
2020-06-03 15:42:53 +0700

    add strconv.atoi code

--- c1-m3-activity/slice.go
+++ c1-m3-activity/slice.go
@@ -3,28 +3,54 @@ package main
 import (
 	"fmt"
 	"sort"
-	"strconv"
+	"unicode"
 )
 
 func main() {
 
-	sli := []int{}
-	for i := 0; true; i++ {
-		fmt.Print("Enter a number/x to exit: ")
-		var input string
-		fmt.Scan(&input)
-		el, error := strconv.Atoi(input)
+	var sli []int
+	for {
+		var inputValue rune
+		fmt.Println("Please enter an integer: ")
+		// duplicated line: https://stackoverflow.com/questions/57734634/why-i-get-a-duplicate-output-when-the-loop-if-there-is-a-scanf
+		num, err := fmt.Scanf("%c\n", &inputValue)
+		if err != nil {
+			fmt.Print(err.Error() + "\n")
+			continue
+		}
 
-		if error != nil {
-			fmt.Println("Program exited")
+		if unicode.IsNumber(inputValue) {
+			// get integer out of ASCII: https://stackoverflow.com/questions/21322173/convert-rune-to-int
+			number := int(inputValue - '0')
+			sli = append(sli, number)
+			sort.SliceStable(sli, func(i, j int) bool { return sli[i] < sli[j] })
+
+			fmt.Printf("Scanned: %d len=%d cap=%d %v \n\n", num, len(sli), cap(sli), sli)
+
+		} else if unicode.IsLetter(inputValue) && inputValue == rune('w') {
+			fmt.Print("bye!\n")
 			break
 		}
 
-		sli = append(sli, el)
-		fmt.Println("Appended:", sli)
-		sort.Ints(sli)
-		fmt.Println("Sorted:", sli)
-
 	}
+
+	// sli := []int{}
+	// for i := 0; true; i++ {
+	// 	fmt.Print("Enter a number/x to exit: ")
+	// 	var input string
+	// 	fmt.Scan(&input)
+	// 	el, error := strconv.Atoi(input)
+
+	// 	if error != nil {
+	// 		fmt.Println("Program exited")
+	// 		break
+	// 	}
+
+	// 	sli = append(sli, el)
+	// 	fmt.Println("Appended:", sli)
+	// 	sort.Ints(sli)
+	// 	fmt.Println("Sorted:", sli)
+
+	// }
 
 }
