457d55e5e23d2d69bc5db0e068fc3e3f2b25d137
hoang <giahoangth@gmail.com>
2020-06-04 16:43:50 +0700

    update c2-m2

--- c2-m2/displacement-calculator.go
+++ c2-m2/displacement-calculator.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
+//fn := GenDisplaceFn(10, 2, 1)
+// fmt.Println(fn(3))
+// fmt.Println(fn(5))
+
 func main() {
-	fn := GenDisplaceFn(10, 2, 0)
-	fmt.Println(fn(3))
-	fmt.Println(fn(5))
+	var d, v, s0, time float64
+	fmt.Printf("Enter intial acceleration:\n")
+	fmt.Scan(&d)
+	fmt.Printf("Enter initial Velocity:\n")
+	fmt.Scan(&v)
+	fmt.Printf("Enter initial Displacement:\n")
+	fmt.Scan(&s0)
+	fn := GenDisplaceFn(d, v, s0)
+	fmt.Printf("Enter Time:\n")
+	fmt.Scan(&time)
+	fmt.Println("Displacement value:", fn(time))
 }
 
 // GenDisplaceFn forming a function with initial value except time of displace formulation
