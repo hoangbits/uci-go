2f7d91457755a2cbe73ca3e4227eaf1798f9d82a
hoang <giahoangth@gmail.com>
2020-07-10 15:42:17 +0700

    c3-m3

--- c3-m3/sortRoutine.go
+++ c3-m3/sortRoutine.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
+	"sync"
 )
 
 func main() {
@@ -14,10 +17,31 @@ func main() {
 	fmt.Println("Enter the integerss")
 	sli := make([]int, n)
 	ReadN(sli, 0, n)
-
+	fmt.Println("Unsorted Main Slice:", sli)
+	// each goroutine sort a subarray and print
+	subSize := int(math.Ceil(float64(len(sli)) / 4.0))
+	var wg sync.WaitGroup
+	wg.Add(4)
+	var mainSli = [][]int{}
+	for i := 0; i < len(sli); i += subSize {
+		var subSli = []int{}
+		if i+subSize >= len(sli) {
+			subSli = sli[i:]
+		} else {
+			subSli = sli[i : i+subSize]
+		}
+		mainSli = append(mainSli, subSli)
+	}
+	for _, subSli := range mainSli {
+		go SortSubArray(subSli, &wg)
+	}
+	wg.Wait()
+	// main goroutine sort data again and print
+	sort.Ints(sli)
+	fmt.Println("Sorted Main Slice:", sli)
 }
 
-// ReadN dfd
+// ReadN cm
 func ReadN(sli []int, i, n int) {
 	if n == 0 {
 		return
@@ -28,4 +52,12 @@ func ReadN(sli []int, i, n int) {
 	// 1,2,3, 4- 3,2,1, 0
 	// e.g subsequent call when n=4 as initial value
 	ReadN(sli, i+1, n-1)
+}
+
+// SortSubArray cm
+func SortSubArray(sli []int, wg *sync.WaitGroup) {
+	fmt.Println("Unsorted Subarray:", sli)
+	sort.Ints(sli)
+	fmt.Println("Sorted Subarray:", sli)
+	wg.Done()
 }
